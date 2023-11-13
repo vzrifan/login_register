ead533a7d3038722222230ccbe117ccd6d1cfb78
vzrifan <vzrifan@gmail.com>
2023-11-14 01:21:28 +0700

    UniqueID for register saved in firebase store

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:testing/firebase_options.dart';
+import 'package:testing/views/register_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
     title: "Flutter Demo",
     theme: ThemeData(primarySwatch: Colors.blue),
-    home: const HomePage(),
+    home: const RegisterView(),
   ));
 }
 
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_udid/flutter_udid.dart';
 import 'package:testing/firebase_options.dart';
+import 'package:testing/views/login_view.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -11,25 +14,82 @@ class RegisterView extends StatefulWidget {
 }
 
 class _RegisterViewState extends State<RegisterView> {
-  late final TextEditingController _email;
-  late final TextEditingController _password;
+  String _id = '';
+  late final TextEditingController _emailController;
 
   @override
   void initState() {
-    _email = TextEditingController();
-    _password = TextEditingController();
     super.initState();
+    _emailController = TextEditingController();
+    print("Init state called");
+    getUID();
+    initializeAppAndCheckDeviceId();
+  }
+
+  Future<void> initializeAppAndCheckDeviceId() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    await getUID();
+    await _checkDeviceIdAndNavigate();
+  }
+
+  Future getUID() async {
+    print("Getting UID...");
+    String udid = await FlutterUdid.udid;
+    setState(() {
+      _id = udid;
+    });
+  }
+
+  Future<void> _checkDeviceIdAndNavigate() async {
+    final password = _id;
+    List<String> storedDeviceId = await getDeviceIdFromFirestore();
+    print(storedDeviceId);
+
+    if (storedDeviceId.contains(password)) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginView()),
+      );
+    }
   }
 
   @override
   void dispose() {
-    _email.dispose();
-    _password.dispose();
+    _emailController.dispose();
     super.dispose();
+  }
+
+  Future<List<String>> getDeviceIdFromFirestore() async {
+    List<String> deviceIds = [];
+
+    // Query the 'users' collection in Firestore
+    QuerySnapshot usersSnapshot =
+        await FirebaseFirestore.instance.collection('users').get();
+
+    // Iterate through each document in the 'users' collection
+    for (QueryDocumentSnapshot userDoc in usersSnapshot.docs) {
+      // Check if the document contains the 'deviceId' field
+      if (userDoc.data() != null && userDoc.data() is Map<String, dynamic>) {
+        // Cast the data to Map<String, dynamic> to access the 'deviceId' field
+        Map<String, dynamic> userData = userDoc.data() as Map<String, dynamic>;
+
+        // Check if the 'deviceId' field is present
+        if (userData.containsKey('deviceId')) {
+          // Add the deviceId to the list
+          deviceIds.add(userData['deviceId']);
+        }
+      }
+    }
+
+    return deviceIds;
   }
 
   @override
   Widget build(BuildContext context) {
+    print(_id);
     return Scaffold(
       appBar: AppBar(
         title: Text("Register"),
@@ -43,38 +103,49 @@ class _RegisterViewState extends State<RegisterView> {
             case ConnectionState.done:
               return Column(
                 children: [
+                  Text(_id),
                   TextField(
-                    controller: _email,
+                    controller: _emailController,
                     decoration:
                         InputDecoration(hintText: "Enter your email here"),
-                    enableSuggestions: false,
-                    autocorrect: false,
                     keyboardType: TextInputType.emailAddress,
-                  ),
-                  TextField(
-                    controller: _password,
-                    decoration:
-                        InputDecoration(hintText: "Enter your password here"),
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
                   ),
                   TextButton(
                     onPressed: () async {
-                      try {
-                        final email = _email.text;
-                        final password = _password.text;
-                        final userCredential = await FirebaseAuth.instance
-                            .createUserWithEmailAndPassword(
-                                email: email, password: password);
-                        print(userCredential);
-                      } on FirebaseAuthException catch (e) {
-                        if(e.code == 'weak-password'){
-                          print("Weak password");
-                        }else if(e.code == 'email-already-in-use'){
-                          print("Email is already in use");
-                        }else if(e.code == 'invalid-email'){
-                          print("Invalid email entered");
+                      final email = _emailController.text;
+                      final password = _id;
+                      List<String> storedDeviceId =
+                          await getDeviceIdFromFirestore();
+                      print(storedDeviceId);
+
+                      if (storedDeviceId.contains(password)) {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => LoginView()),
+                        );
+                      } else {
+                        try {
+                          final userCredential = await FirebaseAuth.instance
+                              .createUserWithEmailAndPassword(
+                            email: email,
+                            password: password,
+                          );
+                          await FirebaseFirestore.instance
+                              .collection('users')
+                              .doc(userCredential.user!.uid)
+                              .set({
+                            'email': email,
+                            'deviceId': password,
+                          });
+                          print(userCredential);
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'weak-password') {
+                            print("Weak password");
+                          } else if (e.code == 'email-already-in-use') {
+                            print("Email is already in use");
+                          } else if (e.code == 'invalid-email') {
+                            print("Invalid email entered");
+                          }
                         }
                       }
                     },
